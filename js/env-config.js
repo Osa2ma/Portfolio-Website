e6298e2eae3e1a21cfc4a767f01dd050bf8320d8
Osama mohamed <120469863+Osa2ma@users.noreply.github.com>
2025-09-09 02:42:06 +0300

    Update env-config.js

--- js/env-config.js
+++ js/env-config.js
@@ -1,8 +1,7 @@
-// This script will be used in the Vercel deployment to inject environment variables
-// It should be referenced before config.vercel.js
-
-// These values will be replaced by Vercel during build time
-window.EMAILJS_SERVICE_ID = '{{EMAILJS_SERVICE_ID}}';
-window.EMAILJS_TEMPLATE_ID = '{{EMAILJS_TEMPLATE_ID}}';
-window.EMAILJS_PUBLIC_KEY = '{{EMAILJS_PUBLIC_KEY}}';
-
+window.CONFIG = {
+  emailjs: {
+    serviceID: "service_x5qn7rm",
+    templateID: "template_ywkcqhw",
+    publicKey: "NVoBJ67b-bq2awnWK"
+  }
+};
