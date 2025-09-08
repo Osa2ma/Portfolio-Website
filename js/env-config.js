5ce55726604c4c2c253624e81ecae8c9618cadbf
Osama mohamed <120469863+Osa2ma@users.noreply.github.com>
2025-09-09 01:49:25 +0300

    Update env-config.js

--- js/env-config.js
+++ js/env-config.js
@@ -4,4 +4,5 @@
 // These values will be replaced by Vercel during build time
 window.EMAILJS_SERVICE_ID = '{{EMAILJS_SERVICE_ID}}';
 window.EMAILJS_TEMPLATE_ID = '{{EMAILJS_TEMPLATE_ID}}';
+window.EMAILJS_PUBLIC_KEY = '{{EMAILJS_PUBLIC_KEY}}';
 
